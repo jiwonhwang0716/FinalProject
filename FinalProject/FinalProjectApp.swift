d9959d746122e0417ccfaf24092e8298a8e3f4eb
Jiwon <jiwonhwang0716@gmail.com>
2023-07-19 23:19:48 +0900

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Jiwon Hwang on 2023/07/19.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
